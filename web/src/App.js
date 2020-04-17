c98d82dd521fb5d8ab38db808f9ffc0d05734028
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-17 15:56:08 -0300

    routes config

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
