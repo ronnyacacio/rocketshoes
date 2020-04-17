e27fc4e07d550d5448e3bc38a9bc029d5bc316de
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-17 17:46:30 -0300

    styles global e header

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
 import Routes from './routes';
+import Header from './components/Header';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
