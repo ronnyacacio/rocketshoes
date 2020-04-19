266c3943e1e32c9db15185ef9687232d7b1b6594
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-19 11:11:11 -0300

    remove products in the cart

--- web/src/store/modules/cart/reducer.js
+++ web/src/store/modules/cart/reducer.js
@@ -13,6 +13,12 @@ export default function cart(state = [], action) {
             amount: 1,
           });
       });
+    case 'REMOVE_FROM_CART':
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.id);
+
+        if (productIndex >= 0) draft.splice(productIndex, 1);
+      });
     default:
       return state;
   }
