a137b5cb5bebca89e93759ec58957235b0b6073b
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-19 09:15:42 -0300

    redux inittial cart

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
