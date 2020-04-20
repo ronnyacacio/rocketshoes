011c891006749c930b5e0e2bd6459e41fec31bce
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-20 14:36:09 -0300

    modulando actions com saga

--- web/src/store/modules/cart/sagas.js
+++ web/src/store/modules/cart/sagas.js
@@ -1,12 +1,28 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select((state) =>
+    state.cart.find((p) => p.id === id)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
