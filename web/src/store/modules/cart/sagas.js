2446ab65bd82bf9c8d0dcfb1b582f45fdd464c17
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-20 14:41:47 -0300

    controling stock with saga

--- web/src/store/modules/cart/sagas.js
+++ web/src/store/modules/cart/sagas.js
@@ -9,8 +9,16 @@ function* addToCart({ id }) {
     state.cart.find((p) => p.id === id)
   );
 
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) return;
+
   if (productExists) {
-    const amount = productExists.amount + 1;
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
