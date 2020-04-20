3dd10b5e39f618891104b080721d7faf76b73434
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-20 14:16:51 -0300

    config redux-saga

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,6 +1,15 @@
-import { createStore } from 'redux';
-import rootReducer from './modules/rootReducer';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(rootReducer);
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const enhancer = applyMiddleware(sagaMiddleware);
+
+const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
